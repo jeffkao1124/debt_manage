8af88b0e9ec359885f64cc8d4388077ccb7d4f72
jeffkao <lssh10517@gmail.com>
2020-09-27 02:50:51 +0800

    make it better

--- app.py
+++ app.py
@@ -33,7 +33,21 @@ class usermessage(db.Model):
     message = db.Column(db.Text)
     birth_date = db.Column(db.TIMESTAMP)
 
+def get_debtPeople(mode):
+    userId = request.values['userId']
+    data_UserData = usermessage.query.filter(usermessage.user_id==userId).filter(usermessage.status=='debt_set')
+    DebtPeopleString=''
+    for _data in data_UserData:
+        DebtPeopleString += _data.nickname.strip(' ') +' '
+    new_list = DebtPeopleString.strip(' ').replace('  ',' ').split(' ')
+    new_list=list(set(new_list)) #刪除重複
 
+    if mode ==1:
+        return len(new_list)
+    elif mode ==2:
+        return new_list
+    else:
+        return 0
 
 @app.route('/',methods=['POST','GET'])
 def index():
@@ -49,7 +63,8 @@ def index():
             save_dic['number'] = count
             save_dic['account'] = _Data.account
             save_dic['message'] = _Data.message
-            save_dic['debtPerson'] ="欠"+str(_Data.group_num)
+            save_dic['debtPerson'] ="欠/"+str(_Data.group_num)
+            save_dic['debtStatus'] = _data.status
             save_list.append(save_dic)
             save_dic = {}
         for _Data in data_BorrowData:
@@ -57,9 +72,30 @@ def index():
             save_dic['number'] = count
             save_dic['account'] = _Data.account
             save_dic['message'] = _Data.message
-            save_dic['debtPerson'] ="借"+str(_Data.group_num)
+            save_dic['debtPerson'] ="借/"+str(_Data.group_num)
+            save_dic['debtStatus'] = _data.status
             save_list.append(save_dic)
             save_dic = {}
+
+        debtPerson_list=get_debtPeople(2)
+        person_total=''
+        total = 0
+        for i in range(get_debtPeople(1)):
+            debtPerson = str(debtPerson_list[i])        
+            for j in range(len(save_list)):
+                historyPerson = str(save_list[j]['debtPerson'])
+                msgStatus = str(save_list[j]['debtStatus'])
+                if debtPerson == historyPerson:
+                    if msgStatus == "owe":
+                        total -= int(history_list[j]['Account'])
+                    if msgStatus == "borrow":
+                        total += int(history_list[j]['Account'])
+            if total > 0:
+                person_total += '我共借'+str(debtPerson)+str(total)+'元'+'\n'
+            if total < 0:
+                total=abs(total)
+                person_total += '我共欠'+str(debtPerson)+str(total)+'元'+'\n'
+            total = 0
       
         
         return render_template('index_form.html',**locals())
